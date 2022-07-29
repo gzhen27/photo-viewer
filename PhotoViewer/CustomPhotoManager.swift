6e9e99696d0d172c8b1e8b7eaf5b2577b582baff
Gray <grayzprogrammer@gmail.com>
2022-07-28 19:56:42 -0500

    creates a CustomPhotoManager Singleton to communicate with the PhotoKit

---
+++ PhotoViewer/CustomPhotoManager.swift
@@ -0,0 +1,12 @@
+//
+//  CustomPhotoManager.swift
+//  PhotoViewer
+//
+//  Created by G Zhen on 7/28/22.
+//
+
+import Foundation
+
+class CustomPhotoManager {
+    static let shared = CustomPhotoManager()
+}
